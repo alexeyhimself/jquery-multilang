3edce2cc2085ef573c1f655e6742b9f9b99eb723
Alexey <ihimself@gmail.com>
2019-03-23 13:01:52 +0300

    Update lang.js
    Added merge_en_lang_with_any_lang to original lib

--- lang.js
+++ lang.js
@@ -1,24 +1,22 @@
-var langs = ['en', 'it'];
-var langCode = '';
-var langJS = null;
+var supported_langs = ['en', 'ru']; 
 
-
-var translate = function (jsdata)
-{	
-	$("[tkey]").each (function (index)
-	{
-		var strTr = jsdata [$(this).attr ('tkey')];
-	    $(this).html (strTr);
-	});
+var translate = function(jsdata) {
+    $("[tkey]").each(function(index) {
+	var strTr = jsdata [$(this).attr('tkey')];
+	$(this).html(strTr);
+    });
 }
 
+// even if custom lang is incompletely translates, english phrases will take place of non-translated parts
+var merge_en_lang_with_any_lang = function(jsdata_any) {
+    $.getJSON('lang/en.json', function(jsdata_en) {
+        var jsdata = $.extend(jsdata_en, jsdata_any);
+        translate(jsdata);
+    });
+}
 
-langCode = navigator.language.substr (0, 2);
-
-if (langCode in langs)
-	$.getJSON('lang/'+langCode+'.json', translate);
+var langCode = navigator.language.substr(0, 2);
+if ($.inArray(langCode, supported_langs))
+	$.getJSON('lang/' + langCode + '.json', merge_en_lang_with_any_lang);
 else
 	$.getJSON('lang/en.json', translate);
-
-
-
